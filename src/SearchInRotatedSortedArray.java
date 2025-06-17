998c9696453778e517958e40c61a897175767eb8
Divyansh <yadavdivyansh445@gmail.com>
2025-06-17 09:33:24 +0530

    SearchInRotatedSortedArrayWithDuplicates

--- src/SearchInRotatedSortedArray.java
+++ src/SearchInRotatedSortedArray.java
@@ -69,5 +69,46 @@ public class SearchInRotatedSortedArray {
             return binarySearch(nums,target,pivot+1,nums.length-1);
 
     }
+    static int findPivotWithDuplicates(int[] arr){
+        if(arr.length==0){
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(mid<end && arr[mid]>arr[mid+1]){
+                return  mid;
+
+            }if(mid >start && arr[mid]<arr[mid-1]){
+                return mid-1;
+
+            }
+            //if elements at middle,start and end are equal then just
+            // skip the duplicates
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]) {
+                //just skip the duplicates
+                // note what if these elements where the pivots.
+                // check if start is pivot
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                // check whether end is pivot
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+                // left side is sorted,so pivot should be in right
+                else if (arr[start]<arr[mid]||arr[start]==arr[mid] && arr[mid]>arr[end]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+
+        }
+        return  -1;
+    }
 
 }
